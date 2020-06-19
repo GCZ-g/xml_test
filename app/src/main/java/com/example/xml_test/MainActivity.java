cd68f4d6acd1e8382cd5eb073da08222e6bce61e
GCZ-g <757354102@qq.com>
2020-06-19 23:02:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xml_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xml_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
